d4ccf315e62b0b6fd104c3613fc9cd2384745771
meegul304 <micha3lbilstein@gmail.com>
2017-02-26 21:36:35 -0500

    New eslint

--- server.js
+++ server.js
@@ -10,82 +10,88 @@ const numCPUS = require("os").cpus().length;
 const cache = {};
 
 if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+	console.log(`Master ${process.pid} is running`);
 
-  new Array(numCPUS).fill(0).forEach((on) => {
-    cluster.fork();
-  });
+	new Array(numCPUS).fill(0).forEach((on) => {
+		cluster.fork();
+	});
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
+	cluster.on("exit", (worker, code, signal) => {
+		console.log(`worker ${worker.process.pid} died`);
+	});
 } else {
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+	const checkUp = (url, callback) => {
+		request(url, (error, response, body) => {
+			if (!error) {
+				callback(response.statusCode);
+			} else {
+				callback(404);
+			}
+		});
+	};
 
-app.get("/", (req, res) => {
-    res.send(compiledIndex());
-});
+	const cacheIt = (url, code) => {
+		const timeChecked = (new Date()).getTime();
+		cache[url] = {
+			time: timeChecked,
+			result: code,
+		};
+	};
 
-app.get("/loaderio-446a472e12c593ff0d019385c2c47066/", (req, res) => {
-        res.send("loaderio-446a472e12c593ff0d019385c2c47066");
-});
+	const checkCache = (url) => {
+		if (cache[url]) {
+			const timeLastChecked = cache[url].time;
+			const currentTime = (new Date()).getTime();
+			if (currentTime - timeLastChecked <= 60000) {//See if this was checked in the last minute.
+				return cache[url].result;
+			} else {
+				return undefined;
+			}
+		} else {
+			return undefined;
+		}
+	};
 
-app.get("/site/*", (req, res) => {
-    const path = req.path.substring(6).toLowerCase();
-    const url = `http://${path}`;
-    let cacheResult;
-    if (cacheResult = checkCache(url)) { //See if we have a cached result.
-        console.log(`Got ${url}, code: ${cacheResult}, from cache.`);
-        const resultText = (cacheResult === 200) ? " is up." : " is down.";
-        res.send(compiledSite({
-            site: path,
-            code: cacheResult,
-            text: resultText
-        }));
-    } else { //We didn't have a recent enough result in the cache.
-        checkUp(url, (result) => {
-            console.log(`${url}: ${result}`);
-            const resultText = (result === 200) ? " is up." : " is down.";
-            res.send(compiledSite({
-                site: path,
-                code: result,
-                text: resultText
-            }));
-            cacheIt(url, result); //Save this result.
-        });
-    }
-});
+	app.listen(port, () => {
+		console.log(`Server listening on port ${port}`);
+	});
 
-const checkUp = (url, callback) => {
-    
-    request(url, (error, response, body) => {
-        if (!error)
-            callback(response.statusCode);
-        else callback(404);
-    });
-};
+	app.get("/", (req, res) => {
+		res.send(compiledIndex());
+	});
 
-function cacheIt(url, code) {
-    const timeChecked = (new Date()).getTime();
-    cache[url] = {
-        time: timeChecked,
-        result: code
-    };
-}
+	// For load-balancing tests.
+	app.get("/loaderio-446a472e12c593ff0d019385c2c47066/", (req, res) => {
+		res.send("loaderio-446a472e12c593ff0d019385c2c47066");
+	});
 
-function checkCache(url) {
-    if (cache[url]) {
-        const timeLastChecked = cache[url].time;
-        const currentTime = (new Date()).getTime();
-        if (currentTime - timeLastChecked <= 60000) //See if this was checked in the last minute.
-            return cache[url].result;
-        else return undefined;
-    } else return undefined;
-}
+	app.get("/site/*", (req, res) => {
+		const path = req.path.substring(6).toLowerCase();
+		const url = `http://${path}`;
+		let cacheResult;
+		if (cacheResult = checkCache(url)) { //See if we have a cached result.
+			console.log(`Got ${url}, code: ${cacheResult}, from cache.`);
+			const resultText = (cacheResult === 200) ? " is up." : " is down.";
+			res.send(compiledSite({
+				site: path,
+				code: cacheResult,
+				text: resultText
+			}));
+		} else { //We didn't have a recent enough result in the cache.
+			checkUp(url, (result) => {
+				console.log(`${url}: ${result}`);
+				const resultText = (result === 200) ? " is up." : " is down.";
+				res.send(compiledSite({
+					site: path,
+					code: result,
+					text: resultText
+				}));
+				cacheIt(url, result); //Save this result.
+			});
+		}
+	});
 
-console.log(`worker ${process.pid} started!`);
+	console.log(`worker ${process.pid} started!`);
 
 }
